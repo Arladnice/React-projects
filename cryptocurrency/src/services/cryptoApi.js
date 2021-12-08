a08edf8690f113d9082c3dbc6b1625a2dd04935b
Denis Arlakov <darlakov@mail.ru>
2021-12-08 16:44:40 +0300

    add

---
+++ cryptocurrency/src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const cryptoHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "ee014b4915mshd9413886ff31177p1bc945jsn0bb0a4e84fb8",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/exchanges";
+
+const createRequest = (url) => ({ url, headers: cryptoHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptoQuery } = cryptoApi;
